9857271366dd11704cda89b014972e811d8b3cab
hariom <htripathi1812@gmail.com>
2023-10-12 16:52:37 +0530

    app.js modified

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 300;
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo App New</h1> <h4>Message: Success Again</h4> <p>Version 1.2</p>');
+  res.send('<h1>Express Demo NodeJS App New</h1> <h4>Message: Success Again</h4> <p>Version 1.2</p>');
 })
 
 app.get('/products', (req, res) => {
